f78c0d7d77ab02bd234bc2bf78c0ecffe62d8294
Tapadipto Banerjee <65211182+Rahul-Malik123@users.noreply.github.com>
2022-10-25 23:19:38 +0530

    My Website

--- mail/contact.php
+++ mail/contact.php
@@ -1,5 +1,6 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+// if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if($_POST['name']) || ($_POST['subject']) || ($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) {
   http_response_code(500);
   exit();
 }
@@ -9,7 +10,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "tapadiptobanerjee357@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
